056d696ee5c6069ecb9705199ce84e61f004cdc5
Marcos Siqueira Junior <junior.crvg@gmail.com>
2016-09-29 19:44:02 -0300

    initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   resources :sessions
   resources :session_blocks
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-   devise_scope :user do 
-    root to: 'static_pages#home'
-    match '/sessions/user', to: 'devise/sessions#create', via: :post
-  end
-  
   resources :students
+  
+  devise_for :users
+  
   get "admin", to: "admin#index", as: :admin 
   namespace :admin do
   	post "turn_professor", as: :turn_professor
-  end	
+  end
+	
 end
